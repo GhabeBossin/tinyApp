0666fa776b259c515f742d10756faa66b58cdd6a
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-16 18:44:01 +0000

    initial commit

---
+++ express_server.js
@@ -0,0 +1,42 @@
+
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (request, response) => {
+  response.send("Hello!");
+});
+
+
+
+app.get("/urls.json", (request, response) => {
+  response.json(urlDatabase);
+});
+
+app.get("/urls", (request, response) => {
+  let templateVars = { urls: urlDatabase };
+  response.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (request, response) => {
+  let templateVars = {
+    shortURL: request.params.id,
+    longURL: urlDatabase[request.params.id]
+  };
+  response.render("urls_show", templateVars);
+});
+
+app.get("/hello", (request, response) => {
+  response.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
