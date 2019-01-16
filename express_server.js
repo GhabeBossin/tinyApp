d9fb3e1bfbdc8dd39d0e923494ca891bd6589c29
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-16 21:16:44 +0000

    URL shortening, create submission form, POST request, generate random number

--- express_server.js
+++ express_server.js
@@ -1,40 +1,57 @@
 
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require('express');
+const app = express();
+const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
+const randomString = require("randomstring");
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+const urlDatabase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
 };
 
-app.get("/", (request, response) => {
-  response.send("Hello!");
+function generateRandomString() {
+  //found package here: https://www.npmjs.com/package/randomstring
+  //console.log(randomString.generate(6));
+  randomString.generate(6);
+}
+generateRandomString();
+
+app.get('/', (request, response) => {
+  response.send('Hello!');
 });
 
-
-
-app.get("/urls.json", (request, response) => {
+app.get('/urls.json', (request, response) => {
   response.json(urlDatabase);
 });
 
-app.get("/urls", (request, response) => {
+app.get('/urls', (request, response) => {
   let templateVars = { urls: urlDatabase };
-  response.render("urls_index", templateVars);
+  response.render('urls_index', templateVars);
 });
 
-app.get("/urls/:id", (request, response) => {
+app.post('/urls', (request, response) => {
+  console.log(request.body);
+  response.send('OK');
+});
+
+app.get('/urls/new',(request, response) => {
+  response.render('urls_new');
+});
+
+app.get('/urls/:id', (request, response) => {
   let templateVars = {
     shortURL: request.params.id,
     longURL: urlDatabase[request.params.id]
   };
-  response.render("urls_show", templateVars);
+  response.render('urls_show', templateVars);
 });
 
-app.get("/hello", (request, response) => {
-  response.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (request, response) => {
+  response.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {
